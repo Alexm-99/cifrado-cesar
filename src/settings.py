32660f7fee6c6b51db0070d757f852b70e5e2b63
Alex Medranda <alexmedranda_99@hotmail.com>
2022-07-25 23:59:34 -0500

    M

--- src/settings.py
+++ src/settings.py
@@ -14,13 +14,14 @@ from pathlib import Path
 import os
 import django_heroku
 
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
